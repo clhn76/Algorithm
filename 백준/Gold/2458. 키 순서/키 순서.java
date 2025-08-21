b708f6310fca0539c6ec62b2477ba744c8955415
Taewon_Ahn <80139845+clhn76@users.noreply.github.com>
2025-08-21 15:52:26 +0900

    [Gold IV] Title: 키 순서, Time: 608 ms, Memory: 37048 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        boolean[][] graph = new boolean[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = true; // a < b 
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (graph[i][k] && graph[k][j]) {
+                        graph[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= N; j++) {
+                if (graph[i][j] || graph[j][i]) { // i 노드가 j보다 작거나 큰 모든 가지수 
+                    cnt++;
+                }
+            }
+            if (cnt == N - 1) { // 해당 노드보다 작거나 큰 노드의 합이 N-1이면 정확한 순서 구할 수 있다.
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
